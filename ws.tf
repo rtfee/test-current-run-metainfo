c4e7eacae536a600f33085e12e798fecb38d98be
Serhii Babak <mermoldy@gmail.com>
2020-05-26 21:27:25 +0300

    Update ws.tf

--- ws.tf
+++ ws.tf
@@ -2,7 +2,7 @@ resource "scalr_workspace" "ws" {
   name = "test3"
   organization = "org-sgpvfvrkj5ao2j0"
   
-  vcs_repo = {
+  vcs_repo {
     identifier = "mermoldy/terraform-google-instance"
     oauth_token_id = "vcs-stplc9ecmtv02j8"
   }
