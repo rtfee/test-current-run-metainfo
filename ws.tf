5577d1b0a7c6e69af78136c4f701b4232da4cfdb
Ryan <ryan.fee625@gmail.com>
2020-05-29 10:25:09 -0400

    Delete ws.tf

--- ws.tf
+++
@@ -1,9 +0,0 @@
-resource "scalr_workspace" "ws" {
-  name = "test3"
-  organization = "org-sgpvfvrkj5ao2j0"
-  
-  vcs_repo {
-    identifier = "mermoldy/terraform-google-instance"
-    oauth_token_id = "vcs-stplc9ecmtv02j8"
-  }
-}
